81b4153adcbcb2055d16d7915a7bb9d832d01480
doech <sie24405@uvg.edu.gt>
2026-03-27 01:51:31 -0600

    primera parte de tareas HU2: login y tokens

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
 import { Controller } from '@nestjs/common';
+import { Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { Body } from '@nestjs/common';
+import { HttpCode } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
+
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK) 
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+}
