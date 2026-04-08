4edb480efdcab9ad2803630064c2ac20ac40d45e
MartinVillatoros <martinguille17@gmail.com>
2026-04-08 08:57:42 -0600

    agrega servicio para crear challenges

--- src/challenges/challenges.service.ts
+++ src/challenges/challenges.service.ts
@@ -1,91 +1,34 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { CreateChallengeDto } from './dto/create-challenge.dto';
-import { UpdateChallengeDto } from './dto/update-challenge.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Challenge } from './entities/challenge.entity';
+import { CreateChallengeDto } from './dto/create-challenge.dto';
 
 @Injectable()
 export class ChallengesService {
   private readonly logger = new Logger(ChallengesService.name);
 
-  private challenges: any[] = []; //Almacenamiento por ahora.
+  
+  constructor(
+    @InjectRepository(Challenge)
+    private challengeRepo: Repository<Challenge>,
+  ) {}
 
-  private participants: any[] = []; //Almacenamiento por ahora.
+  
+  async create(createChallengeDto: CreateChallengeDto, userId: string) {
+    this.logger.log(Creating challenge with name: ${createChallengeDto.name});
 
-  create(createChallengeDto: CreateChallengeDto, userId: number) {
-  this.logger.log(`Creating challenge with name: ${createChallengeDto.name}`);
+    const challenge = this.challengeRepo.create({
+      ...createChallengeDto,
+      created_by_user_id: userId,
+    });
 
-  const createdChallenge = {
-    id: Date.now(),
-    created_by_user_id: userId,
-    ...createChallengeDto,
-  };
-
-  this.challenges.push(createdChallenge); //Almacenamiento por ahora.
-
-  return {
-    message: 'Challenge created successfully',
-    challenge: createdChallenge,
-  };
-}
-
-  findAll() {
-  const userId = 1; // mock
-
-  return this.challenges.map(challenge => {
-    const participants = this.participants.filter(
-      p => p.challengeId === challenge.id
-    );
-
-    const isJoined = participants.some(p => p.userId === userId);
+    const saved = await this.challengeRepo.save(challenge);
 
     return {
-      ...challenge,
-      participants: participants.length,
-      joined: isJoined,
+      message: 'Challenge created successfully',
+      challenge: saved,
     };
-  });
-}
-
-  joinChallenge(userId: number, challengeId: number) {
-  const challenge = this.challenges.find(c => c.id === challengeId);
-
-  if (!challenge) {
-    return { message: 'Challenge not found' };
   }
 
-  const alreadyJoined = this.participants.find(
-    p => p.userId === userId && p.challengeId === challengeId
-  );
-
-  if (alreadyJoined) {
-    return { message: 'User already joined this challenge' };
-  }
-
-  const participation = {
-    userId,
-    challengeId,
-    joinedAt: new Date(),
-  };
-
-  this.participants.push(participation);
-
-  return {
-    message: 'Joined successfully',
-    data: participation,
-  };
-}
-
-  findOne(id: number) {
-    return `This action returns a #${id} challenge`;
-  }
-
-  update(id: number, updateChallengeDto: UpdateChallengeDto) {
-    return `This action updates a #${id} challenge`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} challenge`;
-  }
 }
