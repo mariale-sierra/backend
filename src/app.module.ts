e11ef2d5080459e007f02f3f5a6231a77b2854c3
a1ecsss <estebana.delapena@gmail.com>
2026-04-07 23:36:58 -0600

    cambio en app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-/* import { TypeOrmModule } from '@nestjs/typeorm';*/
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { ChallengesModule } from './challenges/challenges.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    /* TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      synchronize: true,
-    }), */
-    ConfigModule.forRoot({isGlobal: true}),
-    UsersModule,
-    AuthModule,
-    ChallengesModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
