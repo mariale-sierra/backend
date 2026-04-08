596eba5571c47ec9db026008a13f75ca75d8ff44
MartinVillatoros <martinguille17@gmail.com>
2026-04-08 08:45:27 -0600

    crea entidad User con UUID de Supabase y campo email opcional

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,14 @@
-export class User {}
+
+import { Entity, PrimaryColumn, Column } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  id: string; // 👈 UUID de Supabase
+
+  @Column()
+  username: string;
+
+  @Column({ nullable: true })
+  email?: string; // opcional
+}
