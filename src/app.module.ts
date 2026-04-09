a75fd7b7494f6d4ff16b7d657d5409617a6b6858
doech <sie24405@uvg.edu.gt>
2026-04-09 09:17:46 -0600

    port

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { ChallengesModule } from './challenges/challenges.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: 5432,
+      port: 6543,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
