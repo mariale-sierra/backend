6fe4d44ae3432522f21ba66045161f4ce7ff5170
doech <sie24405@uvg.edu.gt>
2026-04-09 01:59:33 -0600

    regreso

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ChallengesModule } from './challenges/challenges.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
 
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: 5432,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-      extra: {
-        family: 4,
-      },
-    })
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule, AuthModule, UsersModule, ChallengesModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        schema: 'havit',
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
   ],
 })
 export class AppModule {}
