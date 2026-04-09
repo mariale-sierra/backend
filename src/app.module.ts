46121d2f8eb4c7ba8eb248d86f29a2c7ac8872d5
doech <sie24405@uvg.edu.gt>
2026-04-09 01:40:12 -0600

    cambios para deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,17 +9,17 @@ import { ChallengesModule } from './challenges/challenges.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
 
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule, AuthModule, UsersModule, ChallengesModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        url: configService.get<string>('DATABASE_URL'),
-        autoLoadEntities: true,
-        schema: 'havit',
-        synchronize: false,
-      }),
-      inject: [ConfigService],
-    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
   ],
 })
 export class AppModule {}
