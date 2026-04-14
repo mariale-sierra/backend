63fedfbd410537f04ddda3cce5f0b1e338bdb3f3
doech <sie24405@uvg.edu.gt>
2026-04-13 22:40:48 -0600

    modificar imports de auth

--- src/challenges/challenges.module.ts
+++ src/challenges/challenges.module.ts
@@ -5,9 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Challenge } from './entities/challenge.entity';
 import { User } from '../users/entities/user.entity';
 import { ChallengeUserMap } from './entities/challenge-user-map.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Challenge, User, ChallengeUserMap])],
+  imports: [
+    TypeOrmModule.forFeature([Challenge, User, ChallengeUserMap]),
+    AuthModule,
+  ],
   controllers: [ChallengesController],
   providers: [ChallengesService],
 })
