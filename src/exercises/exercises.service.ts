45766c7db9e4e676013a45ba55dd7117ee3c79e7
AlejG2005 <droiiipm@hotmail.com>
2026-04-20 18:26:48 -0600

    FEAT: Se creó una seed o pool de ejercicios básicos en el endpoint exercises. Para que no se cree vacío.

--- src/exercises/exercises.service.ts
+++ src/exercises/exercises.service.ts
@@ -1,8 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class ExercisesService {
+export class ExercisesService implements OnModuleInit {
   private exercises: any[] = [];
+
+  //Creación de la pool, 4 ejercicio que aparecen siempre aunque no hayan ejercicios.
+  onModuleInit() {
+    if (this.exercises.length === 0){
+      this.exercises.push(
+        {
+          id: Date.now(),
+          name: 'Push Up',
+          muscle_group: 'chest',
+          difficulty: 'easy',
+        },
+        {
+          id: Date.now() + 1,
+          name: 'Squat',
+          muscle_group: 'legs',
+          difficulty: 'medium',
+        },
+        {
+          id: Date.now() + 2,
+          name: 'Pull Up',
+          muscle_group: 'back',
+          difficulty: 'hard',
+        },
+        {
+          id: Date.now() + 3,
+          name: 'Plank',
+          muscle_group: 'core',
+          difficulty: 'medium',
+        }
+      );
+    }
+  }
 
   create(exerciseDto: any) {
     const exercise = {
