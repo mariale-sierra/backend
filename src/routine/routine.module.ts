1b672a2d76fb05f31e3211cee48fd1ac332a22a4
doech <sie24405@uvg.edu.gt>
2026-05-10 15:30:15 -0600

    fix bug challenges y routines

--- src/routine/routine.module.ts
+++ src/routine/routine.module.ts
@@ -7,14 +7,13 @@ import { Exercise } from '../exercises/entities/exercise.entity';
 
 import { RoutineService } from './routine.service';
 import { RoutineController } from './routine.controller';
+import { Challenge } from '../challenges/entities/challenge.entity';
+import { ChallengesModule } from '../challenges/challenges.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([
-      Routine,
-      RoutineExercise,
-      Exercise, 
-    ]),
+    TypeOrmModule.forFeature([Routine, RoutineExercise, Exercise, Challenge]),
+    ChallengesModule,
   ],
   controllers: [RoutineController],
   providers: [RoutineService],
