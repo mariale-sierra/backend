69534cca8fcf2f56a4c34ade5c04e4eb72c623e3
MartinVillatoros <martinguille17@gmail.com>
2026-05-26 00:09:15 -0600

    registrar modulo de OpenAI en workout log

--- src/workout-log/workout-log.module.ts
+++ src/workout-log/workout-log.module.ts
@@ -24,6 +24,7 @@ import { OpenAiModule } from '../openai/openai.module';
     ]),
     AuthModule,
     WorkoutPostsModule,
+    OpenAiModule,
   ],
   controllers: [WorkoutLogController],
   providers: [WorkoutLogService],
